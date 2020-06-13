6dff89e1a89496c3fa0cd350d975d878f170d8fa
Altaf Sarker <ahsarker@AHSarker-M07.local>
2020-06-12 18:50:05 -0700

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
-var ball;
+var ball, database, position, abc;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
+    abc = database.ref("Ball/position");
+    abc.on("value",readOperation, showError);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
 }
@@ -9,21 +12,31 @@ function setup(){
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        write(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        write(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        write(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        write(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function write(x,y){
+    database.ref("Ball/position").set({
+        x: ball.x + x, 
+        y: ball.y + y
+    })
+}
+function readOperation(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function showError(){
+    console.log(error)
 }
